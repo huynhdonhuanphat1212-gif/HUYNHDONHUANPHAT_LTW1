0d78517b7f995e113233f276eb168a36fa833048
HuynhDoNhuanPhat <HuynhDoNhuanPhat1212@gmail.com>
2026-07-27 14:48:01 +0700

    update index.php, add temp.php

--- temp.php
+++ temp.php
@@ -3,11 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>file temp</title>
 </head>
 <body>
-    <h1>Xin chào A1 </h1>
-    <h1>Xin chào A2 </h1>
-
+    
 </body>
 </html>
